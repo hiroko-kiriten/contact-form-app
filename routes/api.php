7e6d30831aab06d0992f6f1d04bc9e9bff50d385
hiroko ishikawa <kiri310@fuga.ocn.ne.jp>
2026-08-01 11:59:41 +0900

    feat: お問い合わせ削除API実装 #5

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::prefix('v1')->group(function () {
     Route::get('/contacts/{contact}', [ContactController::class, 'show']);
     Route::post('/contacts', [ContactController::class, 'store']);
     Route::put('/contacts/{contact}', [ContactController::class, 'update']);
+    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
 
 
 });
